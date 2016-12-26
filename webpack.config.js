85d1896ba31f72dbdacf1d5cd837d568782f5d8a
Livio Brunner <Livio Brunner>
2016-12-26 18:58:37 +0100

    Release 0.0.54

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
 var webpack = require('webpack');
+var minimize = process.argv.indexOf('--minimize') !== -1;
 module.exports = {
     entry: './src/index.ts',
     output: {
-        // libraryTarget: "var",
-        // library: "mcapi",
-        filename: 'lib/mcapi.min.js'
+        libraryTarget: "var",
+        library: "mcapi",
+        filename: minimize ? 'lib/mcapi.min.js' : 'lib/mcapi.js'
     },
     resolve: {
         extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
     },
-    plugins: [
+    plugins: minimize ? [
         new webpack.optimize.UglifyJsPlugin()
-    ],
+    ] : [],
     module: {
         loaders: [
             { test: /\.ts$/, loader: 'ts-loader' }
