cbc680026180f6343336038efb9ba1b6c98665a4
Livio Brunner <Livio Brunner>
2016-12-26 20:58:00 +0100

    bower release 1.0.0-alpha.1

---
+++ lib/commands/Achievement/index.d.ts
@@ -0,0 +1,6 @@
+import { Achievement } from './Achievement';
+import { IAchievementCommand, AchievementCommand } from './AchievementCommand';
+import { AchievementCommandFrom, IAchievementCommandFrom } from './AchievementCommandFrom';
+import { AchievementCommandTo, IAchievementCommandTo } from './AchievementCommandTo';
+import { AchievementCommandCommand, IAchievementCommandCommand } from './AchievementCommandCommand';
+export { Achievement, AchievementCommand, IAchievementCommand, AchievementCommandFrom, IAchievementCommandFrom, AchievementCommandCommand, IAchievementCommandCommand, AchievementCommandTo, IAchievementCommandTo };
---
+++ lib/commands/index.d.ts
@@ -0,0 +1,3 @@
+import { SummonCommand, ISummonCommand } from "./SummonCommand";
+import { Achievement, IAchievementCommand, AchievementCommand, AchievementCommandCommand, AchievementCommandFrom, AchievementCommandTo, IAchievementCommandFrom, IAchievementCommandTo, IAchievementCommandCommand } from "./Achievement";
+export { SummonCommand, ISummonCommand, Achievement, AchievementCommand, IAchievementCommand, AchievementCommandFrom, IAchievementCommandFrom, AchievementCommandCommand, IAchievementCommandCommand, AchievementCommandTo, IAchievementCommandTo };
