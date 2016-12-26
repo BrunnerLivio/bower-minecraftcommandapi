cd567a2cfd8fe2ebb09125dee1cb313fa76fe3b8
Livio Brunner <Livio Brunner>
2016-12-26 18:09:14 +0100

    bower release 0.0.533

---
+++ lib/core/Mob/index.d.ts
@@ -0,0 +1,4 @@
+import { IMob, Mob } from './Mob';
+import { IMobTag, MobTag } from './MobTag';
+import { IMobTagData, MobTagData } from './MobTagData';
+export { IMob, Mob, IMobTag, MobTag, MobTagData, IMobTagData };
